c0fa627ff70771d52b1daade33c9199e4c855378
Uy Ha <hchanuy@gmail.com>
2025-05-20 08:46:23 +0000

    better error message

--- src/linux/utils.zig
+++ src/linux/utils.zig
@@ -4,17 +4,19 @@ pub fn checkValues(comptime T: type, comptime map: anytype) void {
     for (map) |entry| {
         const field, const macro = entry;
 
-        var events: T = @bitCast(@as(Value, 0));
+        var events: T = std.mem.zeroes(T);
         @field(events, field) = true;
         const raw: Value = @bitCast(events);
 
         const linux_value = @field(linux, macro);
         if (raw != linux_value) {
-            @compileError(
-                "Mismatched between " ++ field ++ " and " ++ macro,
-            );
+            @compileError(std.fmt.comptimePrint(
+                "Mismatched between {s} and {s}: {X} != {X}",
+                .{ field, macro, raw, linux_value },
+            ));
         }
     }
 }
 
+const std = @import("std");
 const linux = @import("linux");
